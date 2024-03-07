4b6d7a1e630dd348eddc7e98cbc8d10d1bda1187
omar <omarkhadrouni2017@gmail.com>
2024-03-07 12:14:56 +0100

    initial commit

---
+++ account-service/src/main/java/net/youssfi/accountservice/web/AccountRestController.java
@@ -0,0 +1,35 @@
+package net.youssfi.accountservice.web;
+
+import lombok.AllArgsConstructor;
+import net.youssfi.accountservice.CustomerRestClient;
+import net.youssfi.accountservice.entities.BankAccount;
+import net.youssfi.accountservice.model.Customer;
+import net.youssfi.accountservice.repository.BankAccountRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class AccountRestController {
+    private BankAccountRepository accountRepository;
+    private CustomerRestClient customerRestClient;
+    @GetMapping("/accounts")
+    public List<BankAccount> accountList(){
+        List<BankAccount> accountList = accountRepository.findAll();
+        accountList.forEach(acc->{
+            acc.setCustomer(customerRestClient.findCustomerById(acc.getCustomerId()));
+        });
+        return accountList;
+    }
+    @GetMapping("/accounts/{id}")
+    public BankAccount bankAccountById(@PathVariable String id){
+        BankAccount bankAccount= accountRepository.findById(id).get();
+        Customer customer=customerRestClient.findCustomerById(bankAccount.getCustomerId());
+        bankAccount.setCustomer(customer);
+        return bankAccount;
+    }
+
+}
